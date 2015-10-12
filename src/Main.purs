636bd9b05fb412bbb56b3127f8393492cfa4284e
quephird <danielle_kefford@hotmail.com>
2015-10-12 01:43:55 -0400

    First cut at moving the player via the key handler.

--- src/Main.purs
+++ src/Main.purs
@@ -1,7 +1,7 @@
 module Main where
 
 import Prelude ( Unit()
-               , (>>=), ($), (+), (-), (++)
+               , (>>=), ($), (+), (-), (++), (#)
                , bind, return, unit )
 
 import Control.Monad.Eff ( Eff() )
@@ -25,19 +25,35 @@ import DOM.Timer ( Timeout()
                  , interval
                  , timeout )
 import Graphics.Canvas ( Canvas() )
+import Optic.Core ( (*~), (^.), (..), (+~)
+                  , LensP()
+                  , lens )
 import Unsafe.Coerce (unsafeCoerce)
 
 import Control.Monad.Eff.Console ( CONSOLE() )
 import Control.Monad.Eff.Console.Unsafe ( logAny )
 
-type Player =
+data Player = Player
   { x :: Number
   , y :: Number
   }
+x = lens (\(Player p) -> p.x)
+         (\(Player p) x' -> Player (p { x = x' }))
 
-type Game =
+data Game = Game
   { player :: Player
   }
+player = lens (\(Game g) -> g.player)
+              (\(Game g) player' -> Game (g { player = player' }))
+
+data Key = Left | Right | SpaceBar | Other
+
+movePlayer key gRef = do
+  let dx = case key of
+             Left -> -1.0
+             Right -> 1.0
+             _ -> 0.0
+  modifySTRef gRef (\g -> g # player .. x +~ dx)
 
 type KeyboardEventMini =
   { keyCode :: Int
@@ -46,28 +62,25 @@ eventToKeyboardEvent :: Event -> KeyboardEventMini
 eventToKeyboardEvent = unsafeCoerce
 
 onKeydown :: forall g eff. STRef g Game
-          -> EventListener (console :: CONSOLE | eff)
+          -> EventListener ( console :: CONSOLE
+                           , st :: ST g  | eff )
 onKeydown gRef = eventListener $ \evt -> do
   let keyboardEvent = eventToKeyboardEvent evt
       code = keyboardEvent.keyCode
-      keyString = case code of
-                    32 -> "space bar"
-                    37 -> "left"
-                    38 -> "up"
-                    39 -> "right"
-                    40 -> "down"
-                    _  -> "other key"
-  logAny $ keyString ++ " pressed!!!"
+      key = case code of
+              32 -> SpaceBar
+              37 -> Left
+              39 -> Right
+              _  -> Other
+  movePlayer key gRef
 
 setup :: Game
-setup = { player: { x: 200.0, y: 200.0 }}
+setup = Game { player: Player { x: 200.0, y: 200.0 } }
 
 update :: forall eff g. STRef g Game
        -> Eff ( console :: CONSOLE
               , st :: ST g | eff ) Unit
 update gRef = do
-  modifySTRef gRef (\g -> { player: { x: g.player.x + 1.0
-                                    , y: g.player.x - 1.0 }})
   return unit
 
 render :: forall eff g. STRef g Game
@@ -78,6 +91,7 @@ render :: forall eff g. STRef g Game
 render gRef = do
   timeout 50 $ do
     g <- readSTRef gRef
+    logAny $ g ^. player .. x
     render gRef
 
 gameLoop :: forall eff g. STRef g Game
