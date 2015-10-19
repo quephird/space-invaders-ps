e8112be9a5a5e7c6fadc7969bc754eb6fdf8ffca
quephird <danielle_kefford@hotmail.com>
2015-10-19 00:46:09 -0400

    Moved all rendering code to new module.

--- src/Main.purs
+++ src/Main.purs
@@ -4,21 +4,14 @@ import Prelude ( Functor, Unit()
                , (>>=), (<$>), ($), (+), (-), (*), (++), (#)
                , bind, mod, return, unit )
 
-import Control.Monad.Eff ( Eff()
-                         , foreachE )
+import Control.Monad.Eff ( Eff() )
 import Control.Monad.ST ( ST()
                         , STRef()
                         , modifySTRef
                         , newSTRef
                         , readSTRef )
-import Data.Array ( (!!), index, length )
-import Data.Date ( Now()
-                 , nowEpochMilliseconds )
-import Data.Int ( fromNumber )
-import Data.Maybe ( Maybe(..) )
-import Data.Maybe.Unsafe ( fromJust )
+import Data.Date ( Now() )
 import Data.Nullable ( toMaybe )
-import Data.Time ( Seconds(..), toSeconds )
 import DOM ( DOM() )
 import DOM.Event.EventTarget ( EventListener()
                              , addEventListener
@@ -38,19 +31,8 @@ import DOM.Timer ( Timeout()
                  , Timer()
                  , interval
                  , timeout )
-import Graphics.Canvas ( Canvas()
-                       , CanvasImageSource()
-                       , Context2D()
-                       , Rectangle()
-                       , drawImage
-                       , fillRect
-                       , getCanvasElementById
-                       , getContext2D
-                       , rect
-                       , setFillStyle
-                       )
+import Graphics.Canvas ( Canvas() )
 import Optic.Core ( (^.), (..), (+~) )
-import Math ( floor )
 import Unsafe.Coerce (unsafeCoerce)
 
 import Control.Monad.Eff.Console ( CONSOLE() )
@@ -61,6 +43,7 @@ import qualified Data.Game.Game as G
 import qualified Data.Game.Invader as I
 import qualified Data.Game.Player as P
 import qualified Data.Game.Sprites as S
+import qualified Render as R
 
 data Key = Left | Right | SpaceBar | Other
 
@@ -96,55 +79,6 @@ update :: forall eff g. STRef g G.Game
 update gRef = do
   return unit
 
--- TODO: Move all rendering stuff to new Render module
-chooseSprite sprites (Seconds s) =
-  let n = length sprites
-      idx = (fromJust $ fromNumber $ floor $ s * 2.0) `mod` 2
-  in
-    fromJust $ sprites !! idx
-
-renderEnemies ctx g = do
-  currentTime <- nowEpochMilliseconds
-  let invaderSprites = g ^. G.invaderSprites
-      invaders = g ^. G.invaders
-      secondsIntoGame = toSeconds $ currentTime - (g ^. G.startTime)
-      sprite = chooseSprite invaderSprites secondsIntoGame
-  foreachE invaders $ \i -> do
-    drawImage ctx
-              sprite
-              (i ^. I.x)
-              (i ^. I.y)
-    return unit
-
-renderPlayer ctx g = do
-  drawImage ctx
-            (g ^. G.playerSprite)
-            (g ^. G.playerX)
-            (g ^. G.playerY)
-  return unit
-
-render :: forall eff g. STRef g G.Game
-       -> Eff ( canvas :: Canvas
-              , console :: CONSOLE
-              , now :: Now
-              , st :: ST g
-              , timer :: Timer | eff ) Timeout
-render gRef = do
-  Just canvas <- getCanvasElementById "canvas"
-  ctx <- getContext2D canvas
-  timeout 50 $ do
-    g <- readSTRef gRef
-
-    setFillStyle "#000000" ctx
-    fillRect ctx { x: 0.0
-                 , y: 0.0
-                 , w: 800.0
-                 , h: 600.0}
-
-    renderEnemies ctx g
-    renderPlayer ctx g
-    render gRef
-
 gameLoop :: forall eff g. STRef g G.Game
          -> Eff ( canvas :: Canvas
                 , console :: CONSOLE
@@ -153,7 +87,7 @@ gameLoop :: forall eff g. STRef g G.Game
                 , timer :: Timer | eff ) Timeout
 gameLoop gRef = do
   interval 200 $ update gRef
-  render gRef
+  R.render gRef
 
 main :: forall eff g. Eff ( canvas :: Canvas
                           , console :: CONSOLE
---
+++ src/Render.purs
@@ -0,0 +1,75 @@
+module Render where
+
+import Prelude ( ($), (-), (*)
+               , bind, mod, return, unit )
+
+import Control.Monad.Eff ( Eff()
+                         , foreachE )
+import Control.Monad.ST ( ST(), STRef()
+                        , readSTRef )
+import Data.Array ( (!!), index, length )
+import Data.Date ( Now()
+                 , nowEpochMilliseconds )
+import Data.Int ( fromNumber )
+import Data.Maybe ( Maybe(..) )
+import Data.Maybe.Unsafe ( fromJust )
+import Data.Time ( Seconds(..), toSeconds )
+import DOM.Timer ( Timeout(), Timer()
+                 , timeout )
+import Graphics.Canvas ( Canvas(), CanvasImageSource(), Context2D(), Rectangle()
+                       , drawImage, fillRect, getCanvasElementById
+                       , getContext2D, rect, setFillStyle
+                       )
+import Math ( floor )
+import Optic.Core ( (^.) )
+
+import qualified Data.Game.Game as G
+import qualified Data.Game.Invader as I
+
+-- TODO: Move all rendering stuff to new Render module
+chooseSprite sprites (Seconds s) =
+  let n = length sprites
+      idx = (fromJust $ fromNumber $ floor $ s * 2.0) `mod` 2
+  in
+    fromJust $ sprites !! idx
+
+renderEnemies ctx g = do
+  currentTime <- nowEpochMilliseconds
+  let invaderSprites = g ^. G.invaderSprites
+      invaders = g ^. G.invaders
+      secondsIntoGame = toSeconds $ currentTime - (g ^. G.startTime)
+      sprite = chooseSprite invaderSprites secondsIntoGame
+  foreachE invaders $ \i -> do
+    drawImage ctx
+              sprite
+              (i ^. I.x)
+              (i ^. I.y)
+    return unit
+
+renderPlayer ctx g = do
+  drawImage ctx
+            (g ^. G.playerSprite)
+            (g ^. G.playerX)
+            (g ^. G.playerY)
+  return unit
+
+render :: forall eff g. STRef g G.Game
+       -> Eff ( canvas :: Canvas
+              , now :: Now
+              , st :: ST g
+              , timer :: Timer | eff ) Timeout
+render gRef = do
+  Just canvas <- getCanvasElementById "canvas"
+  ctx <- getContext2D canvas
+  timeout 50 $ do
+    g <- readSTRef gRef
+
+    setFillStyle "#000000" ctx
+    fillRect ctx { x: 0.0
+                 , y: 0.0
+                 , w: 800.0
+                 , h: 600.0}
+
+    renderEnemies ctx g
+    renderPlayer ctx g
+    render gRef
